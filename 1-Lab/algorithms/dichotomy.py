196f93aeecd54dfef18aaf9172795b5316729bf6
JoJukov <ivanzhukov2002@mail.ru>
2021-03-24 23:07:53 +0300

    added dichotomy + update method

--- 1-Lab/algorithms/dichotomy.py
+++ 1-Lab/algorithms/dichotomy.py
@@ -1,7 +1,30 @@
-from method import Method
+from algorithms.method import *
 
 
 class DichotomyMethod(Method):
     # implementation here
+
+    def __init__(self, function, eps, left, right):
+        super().__init__(function, eps, left, right)
+
     def run(self):
-        pass
+        left = self.left
+        right = self.right
+        while right - left >= self.eps:
+            self.iterations += 1
+            x1 = ((right + left) / 2) - self.eps / 2 * 0.8
+            x2 = ((right + left) / 2) + self.eps / 2 * 0.8
+            f_x1 = self.function(x1)
+            f_x2 = self.function(x2)
+            self.function_calls += 2
+            if f_x1 == f_x2:
+                left = x1
+                right = x2
+            elif f_x1 > f_x2:
+                left = x1
+            elif f_x1 < f_x2:
+                right = x2
+            else:
+                assert IOError("idk how you did this but your I/O quite bad")
+        self.answer_point = (right + left) / 2
+        self.answer = self.function(self.answer_point)
--- 1-Lab/algorithms/method.py
+++ 1-Lab/algorithms/method.py
@@ -4,6 +4,10 @@ class Method:
         self.eps = eps
         self.left = left
         self.right = right
+        self.answer = None
+        self.answer_point = None
+        self.iterations = 0
+        self.function_calls = 0
 
     def run(self):
         pass
