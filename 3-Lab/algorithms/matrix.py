055dbf5c518658d481999a1ac8931624fea9b787
Neli <neliitsme14@gmail.com>
2021-05-12 11:04:44 +0300

    update .gitignore & add 3rd lab cornerstone

---
+++ 3-Lab/algorithms/matrix.py
@@ -0,0 +1,8 @@
+
+class Matrix:
+    def __init__(self, args: list):
+        self.matrix = args
+        self.decomposed_matrix = []
+
+    def LU_Decompose(self):
+        pass #Todo
---
+++ 3-Lab/algorithms/method.py
@@ -0,0 +1,12 @@
+from matrix import Matrix
+
+class Method:
+    def __init__(self, matrix: Matrix):
+        self.matrix = matrix
+        self.iterations = 0
+        self.function_calls = 0
+        self.answer = None
+
+    def run(self):
+        pass
+
