a3f5848c46490228def27cd8181f3aacff69cb7c
Neli <neliitsme14@gmail.com>
2021-05-13 16:07:14 +0300

    add gauss method

--- 3-Lab/algorithms/method.py
+++ 3-Lab/algorithms/method.py
@@ -1,12 +1,14 @@
-from matrix import Matrix
+from .matrix import Matrix
+import scipy.sparse as scisp
+
 
 class Method:
-    def __init__(self, matrix: Matrix):
+    def __init__(self, matrix: Matrix, m_equals: scisp.csr_matrix):
         self.matrix = matrix
+        self.m_equals = m_equals
         self.iterations = 0
         self.function_calls = 0
-        self.answer = None
+        self.answer = scisp.lil_matrix((1, self.matrix.n))
 
     def run(self):
         pass
-
